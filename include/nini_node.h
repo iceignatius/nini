2345fe87d20b79b6a63f97cf7e6f3f026d720233
Ignatius <ice.emissary@gmail.com>
2017-07-13 13:38:07 +0800

    加入 nini_node 模組的 C++ 包裝元件 TNode。

--- include/nini_node.h
+++ include/nini_node.h
@@ -9,6 +9,11 @@
 #define _NINI_NODE_H_
 
 #include <stdbool.h>
+
+#ifdef __cplusplus
+#include <string>
+#endif
+
 #include "nini_type.h"
 
 #ifdef __cplusplus
@@ -268,6 +273,104 @@ void nini_node_unlink    (nini_node_t *self);
 
 #ifdef __cplusplus
 }  // extern "C"
+#endif
+
+#ifdef __cplusplus
+
+namespace nini
+{
+
+/**
+ * @brief C++ wrapper of nini_node_t.
+ */
+class TNode : protected nini_node_t
+{
+protected:
+    ~TNode() {}
+
+public:
+    /// The same as nini_node_create_section.
+    static TNode* CreateSection(const std::string &name)
+    { return (TNode*) nini_node_create_section(name.c_str()); }
+
+    /// The same as nini_node_create_string.
+    static TNode* CreateString(const std::string &name, const std::string &value)
+    { return (TNode*) nini_node_create_string(name.c_str(), value.c_str()); }
+
+    /// The same as nini_node_create_decimal.
+    static TNode* CreateDecimal(const std::string &name, long value)
+    { return (TNode*) nini_node_create_decimal(name.c_str(), value); }
+
+    /// The same as nini_node_create_hexa.
+    static TNode* CreateHexa(const std::string &name, long value)
+    { return (TNode*) nini_node_create_hexa(name.c_str(), value); }
+
+    /// The same as nini_node_create_float.
+    static TNode* CreateFloat(const std::string &name, double value)
+    { return (TNode*) nini_node_create_float(name.c_str(), value); }
+
+    /// The same as nini_node_create_bool.
+    static TNode* CreateBool(const std::string &name, bool value)
+    { return (TNode*) nini_node_create_bool(name.c_str(), value); }
+
+    /// The same as nini_node_create_null.
+    static TNode* CreateNull(const std::string &name)
+    { return (TNode*) nini_node_create_null(name.c_str()); }
+
+    /// Destruct and release object instance, the same as nini_node_release.
+    void Release() { nini_node_release(this); }
+
+public:
+    /// The same as nini_node_get_name.
+    std::string GetName() const { return nini_node_get_name(this); }
+    /// The same as nini_node_get_type.
+    TType GetType() const { return nini_node_get_type(this); }
+    /// The same as nini_node_have_value.
+    bool HaveValue() const { return nini_node_have_value(this); }
+    /// The same as nini_node_have_child.
+    bool HaveChild() const { return nini_node_have_child(this); }
+
+    /// The same as nini_node_get_prev_sibling.
+    TNode* GetPrevSibling() { return (TNode*) nini_node_get_prev_sibling(this); }
+    /// The same as nini_node_get_next_sibling.
+    TNode* GetNextSibling() { return (TNode*) nini_node_get_next_sibling(this); }
+    /// The same as nini_node_get_prev_sibling_c.
+    const TNode* GetPrevSibling() const { return (const TNode*) nini_node_get_prev_sibling_c(this); }
+    /// The same as nini_node_get_next_sibling_c.
+    const TNode* GetNextSibling() const { return (const TNode*) nini_node_get_next_sibling_c(this); }
+
+    /// The same as nini_node_get_first_child.
+    TNode* GetFirstChild() { return (TNode*) nini_node_get_first_child(this); }
+    /// The same as nini_node_get_last_child.
+    TNode* GetLastChild() { return (TNode*) nini_node_get_last_child(this); }
+    /// The same as nini_node_find_child.
+    TNode* FindChild(const std::string &name) { return (TNode*) nini_node_find_child(this, name.c_str()); }
+
+    /// The same as nini_node_get_first_child_c.
+    const TNode* GetFirstChild() const { return (const TNode*) nini_node_get_first_child_c(this); }
+    /// The same as nini_node_get_last_child_c.
+    const TNode* GetLastChild() const { return (const TNode*) nini_node_get_last_child_c(this); }
+    /// The same as nini_node_find_child_c.
+    const TNode* FindChild(const std::string &name) const { return (const TNode*) nini_node_find_child_c(this, name.c_str()); }
+
+    /// Ths same as nini_node_get_string.
+    std::string GetString() const { return nini_node_get_string(this); }
+    /// Ths same as nini_node_get_integer.
+    long GetInteger() const { return nini_node_get_integer(this); }
+    /// Ths same as nini_node_get_float.
+    double GetFloat() const { return nini_node_get_float(this); }
+    /// Ths same as nini_node_get_bool.
+    bool GetBool() const { return nini_node_get_bool(this); }
+
+    /// Ths same as nini_node_link_child.
+    bool LinkChild(TNode *node) { return nini_node_link_child(this, node); }
+    /// The same as nini_node_unlink.
+    void Unlink() { nini_node_unlink(this); }
+
+};
+
+}
+
 #endif
 
 #endif
