1a9e8337e0416d74fcc12e56f348fe7636bd5939
Ignatius <ice.emissary@gmail.com>
2017-07-13 14:47:19 +0800

    加入預設的專案建置腳本。

---
+++ build-and-test.sh
@@ -0,0 +1,15 @@
+#
+# Project build and test.
+#
+#!/bin/sh
+
+./gendoc.sh
+
+test -d install && rm -r install
+test -d build || mkdir build
+cd build && rm -r ./*
+
+cmake -DCMAKE_INSTALL_PREFIX=`pwd`/../install -DBUILD_SHARED_LIBS=no .. && \
+    make && \
+    make test && \
+    make install
