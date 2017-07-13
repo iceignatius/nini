49bff650b6c04bb6e3895dc37ef6744f6618b6f4
Ignatius <ice.emissary@gmail.com>
2017-07-13 14:45:20 +0800

    加入文件建置腳本。

---
+++ gendoc.sh
@@ -0,0 +1,15 @@
+#
+# Generate documents.
+#
+#!/bin/sh
+
+doxygen
+
+sed -i -e 's,begin{document},usepackage{CJKutf8}\n\\begin{document}\n\\begin{CJK}{UTF8}{bsmi},' \
+    doc/latex/refman.tex
+sed -i -e 's,end{document},end{CJK}\n\\end{document},' \
+    doc/latex/refman.tex
+
+cd doc/latex && \
+    make && \
+    cp refman.pdf "../Nested INI Library Manual.pdf"
